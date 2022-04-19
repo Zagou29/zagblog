3cd51d0e89f4b23791e5a1e9818bc88864916984
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2022-04-19 19:43:40 +0200

    Rajouté Break dans les cases pour les fleches precedentes de Photos.js

--- photos.js
+++ photos.js
@@ -17,7 +17,7 @@ const tab_titre = [
   { id: "a708", titre: "Les années 70" },
   { id: "a80", titre: "Les années 80" },
   { id: "a90", titre: "Les années 90" },
-  { id: "chgv", titre: "Changements " }
+  { id: "chgv", titre: "Changements " },
 ];
 const val_titre = tab_titre.find((val) => val.id === val_trans);
 localStorage.removeItem("data");
@@ -78,15 +78,18 @@ const av_ar = () => {
   fleches.forEach((el) => {
     el.addEventListener("click", (e) => {
       switch (el) {
-        case fleches[0] /* aller à position gauche de l'image- largeur de l'image*/: {
+        /* aller à position gauche de l'image- largeur de l'image*/
+        case fleches[0]: {
           boiteImg.scrollTo({
             left: boiteImg.scrollLeft - boiteImg.offsetWidth,
           });
+          break;
         }
         case fleches[1]: {
           boiteImg.scrollTo({
             left: boiteImg.scrollLeft + boiteImg.offsetWidth,
           });
+          break;
         }
       }
       e.stopPropagation();
