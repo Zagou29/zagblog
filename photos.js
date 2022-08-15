9414b85b3e620056b772eaaa5b496178a4697d3d
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2022-08-15 17:44:48 +0200

    Simplifié Photos .js

--- photos.js
+++ photos.js
@@ -5,12 +5,12 @@ import { go_fullScreen, stop_fullScreen } from "./fullScreen.js";
 const val_trans = localStorage.getItem("data");
 const val = document.querySelector(".transval");
 const fix_fond = document.querySelector(".envel");
-const full =fix_fond.querySelector(".fullscreen");
-const fleches =fix_fond.querySelectorAll(".fleches");
-const boiteImg =fix_fond.querySelector(".image");
-const stop_prec =fix_fond.querySelector(".prec");
-const stop_suiv =fix_fond.querySelector(".suiv");
-const list_img = [... boiteImg.getElementsByClassName(`${val_trans}`)];
+const full = fix_fond.querySelector(".fullscreen");
+const fleches = fix_fond.querySelectorAll(".fleches");
+const boiteImg = fix_fond.querySelector(".image");
+const stop_prec = fix_fond.querySelector(".prec");
+const stop_suiv = fix_fond.querySelector(".suiv");
+const list_img = [...boiteImg.getElementsByClassName(`${val_trans}`)];
 const tab_titre = [
   { id: "avion", titre: "Avions 14-18" },
   { id: "guerre", titre: "Guerre 14-18" },
@@ -32,64 +32,37 @@ const tab_titre = [
 const val_titre = tab_titre.find((val) => val.id === val_trans);
 localStorage.removeItem("data");
 val.textContent = val_titre.titre;
-/* selectionner les images selon val_trans */
-list_img.forEach((list) => {
-  list.classList.add("show");
-});
-
-/* -------------------------------- */
+/* afficher les images selon val_trans */
+list_img.forEach((list) => list.classList.add("show"));
 /* --------------------------------------------- */
-/* Zoom quand on clicke sur une image en changeant les classes */
-let zoome = false;
-const zoom = (e) => {
-  zoome = zoome === true ? false : true;
-  /* revenir en mode normal si on est en fullscreen +retour images */
-  stop_fullScreen();
-  /* montrer les flèches */
-  fleches.forEach((fl) => fl.classList.toggle("show_grid"));
-  /* montrer les fleche f pour fullscreen */
-  const alert = () => full.classList.remove("show_grid");
-  if (zoome) {
-    full.classList.add("show_grid");
-    setTimeout(alert, 5000);
+/* fonction qui ajoute ou enleve l'icone stop sur les fleches */
+const montreStop = (condition, el) => {
+  if (condition) el.classList.add("show");
+  else {
+    el.classList.remove("show");
   }
-
-  /* ramener toutes les images en plein ecran et definelemnt horizontal */
-  boiteImg.classList.toggle("image_mod");
-  fix_fond.classList.toggle("envel_mod");
-  /* pointer sur l'image sur laquelle on a cliqué */
-  boiteImg.scrollTo({
-    left: e.target.offsetLeft,
-  });
-
-  /* rajouter le stop au debut et la la fin des images */
-  boiteImg.addEventListener("scroll", () => {
-    if (boiteImg.scrollLeft === 0) stop_prec.classList.add("show");
-    else {
-      stop_prec.classList.remove("show");
-    }
-    if (boiteImg.scrollLeft === boiteImg.scrollWidth - boiteImg.offsetWidth)
-      stop_suiv.classList.add("show");
-    else {
-      stop_suiv.classList.remove("show");
-    }
-  });
 };
-
-/* --------------------------------------------- */
-/* ---utilisation des fleches pour derouler les images*/
+/* montre l'icone stop debut ou l'icone stop fin ou efface */
+const stopFleches = () => {
+  montreStop(boiteImg.scrollLeft === 0, stop_prec);
+  montreStop(
+    boiteImg.scrollLeft === boiteImg.scrollWidth - boiteImg.offsetWidth,
+    stop_suiv
+  );
+};
+/* ---utilisation des icones fleches pour derouler les images*/
 const av_ar = () => {
-  fleches.forEach((el) => {
+  fleches.forEach((el, index) => {
     el.addEventListener("click", (e) => {
-      switch (el) {
+      switch (index) {
         /* aller à position gauche de l'image- largeur de l'image*/
-        case fleches[0]: {
+        case 0: {
           boiteImg.scrollTo({
             left: boiteImg.scrollLeft - boiteImg.offsetWidth,
           });
           break;
         }
-        case fleches[1]: {
+        case 1: {
           boiteImg.scrollTo({
             left: boiteImg.scrollLeft + boiteImg.offsetWidth,
           });
@@ -100,9 +73,34 @@ const av_ar = () => {
     });
   });
 };
+/* Zoom quand on clicke sur une image en changeant les classes */
+let zoome = false;
+const alert = () => full.classList.remove("show_grid");
+const zoom = (e) => {
+  zoome = zoome === true ? false : true;
+  /* revenir en mode normal si on est en fullscreen +retour images */
+  stop_fullScreen();
+  /* montrer les flèches */
+  fleches.forEach((fl) => fl.classList.toggle("show_grid"));
+  /* ramener toutes les images en plein ecran et definelemnt horizontal */
+  boiteImg.classList.toggle("image_mod");
+  fix_fond.classList.toggle("envel_mod");
+  if (zoome) {
+    /* pointer sur l'image sur laquelle on a cliqué */
+    boiteImg.scrollTo({ left: e.target.offsetLeft });
+    /* montrer les fleche f pour fullscreen , puis effacer en 5s*/
+    full.classList.add("show_grid");
+    setTimeout(alert, 5000);
+    /* rajouter le stop au debut et la la fin des images au depart, puis au scroll */
+    stopFleches();
+    boiteImg.addEventListener("scroll", () => stopFleches());
+    /* gestion des fleches pour derouler les images horizontalement */
+    av_ar();
+  }
+};
 
 /* ----utilisation des touches clavier */
-const drGa = (gauche, droite, retour, esc, fs) => {
+const drGa = (gauche, droite, retour, fs) => {
   document.addEventListener("keydown", (e) => {
     if (e.preventDefault()) return;
     /* image de droite ou image de gauche */
@@ -116,14 +114,10 @@ const drGa = (gauche, droite, retour, esc, fs) => {
         boiteImg.scrollTo({ left: boiteImg.scrollLeft + boiteImg.offsetWidth });
         break;
       }
-      /* retour à Index.html */
+      /* retour à Index.html ou au mur d'images*/
       case retour: {
-        window.location = "./index.html";
-        break;
-      }
-      /* Toggles ecrans */
-      case esc: {
-        zoom(e);
+        if (zoome) zoom(e);
+        else window.location = "./index.html";
         break;
       }
       /* Toggle Fullscreen */
@@ -137,8 +131,5 @@ const drGa = (gauche, droite, retour, esc, fs) => {
 };
 /* -----------programme------------------------------- */
 /* zoom quand on clique sur une image */
-list_img.forEach((img) => {
-  img.addEventListener("click", (e) => zoom(e));
-});
-av_ar();
-drGa("ArrowLeft", "ArrowRight", "Enter", "Escape", "KeyF");
+list_img.forEach((img) => img.addEventListener("click", (e) => zoom(e)));
+drGa("ArrowLeft", "ArrowRight", "Enter", "KeyF");
